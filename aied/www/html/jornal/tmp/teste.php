e4bce6f0bdd355c67f4cd06a431ed4d619096a82
Wagner <kazu_hiko@hotmail.com>
2023-08-08 19:05:09 -0300

    do while true

--- aied/www/html/jornal/tmp/teste.php
+++ aied/www/html/jornal/tmp/teste.php
@@ -8,11 +8,21 @@
             //    $a = random_int(0, 100);
             //}
 
+            //do{
+            //    $a = random_int(0, 100);
+            //    echo strval($a) . ' é menor que 90<br>';
+            //}
+            //while($a < 90);
+
             do{
                 $a = random_int(0, 100);
+            
+                if($a >= 90){
+                    break;
+                }
                 echo strval($a) . ' é menor que 90<br>';
             }
-            while($a < 90);
+            while(true);
 
         ?>
     </body>
