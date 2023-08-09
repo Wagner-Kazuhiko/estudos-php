dc39ee291e368730baa9e1ac44d9a64089c2ca02
Wagner <kazu_hiko@hotmail.com>
2023-08-09 18:24:50 -0300

    json php

--- aied/www/html/jornal/tmp/teste.php
+++ aied/www/html/jornal/tmp/teste.php
@@ -2,7 +2,10 @@
     <body>
         <?php
 
-            $a = json_decode( json_encode(array("id"=>1, "nome"=>"Kaka", "contatos"=>array("4555-8888", "9999-7777"))));
+            $buffer = array("id"=>1, "nome"=>"Kaka", "contatos"=>array("4555-8888", "9999-7777"));
+            $a = json_decode( json_encode($buffer));
+            print_r($buffer);
+            echo "<br>";
             
             print_r($a);
 
