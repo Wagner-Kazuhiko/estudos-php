73af5a8a37a1fc504f1e1a540323fe2ff60355e8
Wagner <kazu_hiko@hotmail.com>
2023-08-09 18:14:28 -0300

    arrays

--- aied/www/html/jornal/tmp/teste.php
+++ aied/www/html/jornal/tmp/teste.php
@@ -2,27 +2,10 @@
     <body>
         <?php
 
-            //$a = random_int(0, 100);
-            //while($a < 90){
-            //    echo strval($a) . ' é menor que 90<br>';
-            //    $a = random_int(0, 100);
-            //}
-
-            //do{
-            //    $a = random_int(0, 100);
-            //    echo strval($a) . ' é menor que 90<br>';
-            //}
-            //while($a < 90);
-
-            do{
-                $a = random_int(0, 100);
-            
-                if($a >= 90){
-                    break;
-                }
-                echo strval($a) . ' é menor que 90<br>';
-            }
-            while(true);
+            $a = array("a", "b", "c", "d");
+            array_push($a, 'f');
+            $a[] = 'e';
+            print_r($a);
 
         ?>
     </body>
