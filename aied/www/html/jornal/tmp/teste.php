c28fc9f9d7c2fed3a54661e4d414da7cba992279
Wagner <kazu_hiko@hotmail.com>
2023-08-10 17:59:18 -0300

    obj tipo json php

--- aied/www/html/jornal/tmp/teste.php
+++ aied/www/html/jornal/tmp/teste.php
@@ -2,12 +2,22 @@
     <body>
         <?php
 
-            $buffer = array("id"=>1, "nome"=>"Kaka", "contatos"=>array("4555-8888", "9999-7777"));
-            $a = json_decode( json_encode($buffer));
-            print_r($buffer);
+            $arr = array("id"=>1, "nome"=>"Kaka", "contatos"=>array("4555-8888", "9999-7777"));
+            $obj = json_decode( json_encode($arr));
+
+            print_r($arr);
+            echo "<br>";
+
+            print_r($obj);
+            echo "<br>";
+
+            echo $arr['nome'];
+            echo "<br>";
+
+            echo $obj->nome;
             echo "<br>";
             
-            print_r($a);
+
 
         ?>
     </body>
