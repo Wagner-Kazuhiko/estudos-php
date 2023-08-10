c8e544735dc09a7791f641c6b822c3e306667cdc
Wagner <kazu_hiko@hotmail.com>
2023-08-10 18:15:13 -0300

    obj tipo json php com loop

--- aied/www/html/jornal/tmp/teste.php
+++ aied/www/html/jornal/tmp/teste.php
@@ -5,17 +5,9 @@
             $arr = array("id"=>1, "nome"=>"Kaka", "contatos"=>array("4555-8888", "9999-7777"));
             $obj = json_decode( json_encode($arr));
 
-            print_r($arr);
-            echo "<br>";
-
-            print_r($obj);
-            echo "<br>";
-
-            echo $arr['nome'];
-            echo "<br>";
-
-            echo $obj->nome;
-            echo "<br>";
+            for($i = 0; $i < count($arr['contatos']); $i++){
+                echo 'Contato: ' . $arr['contatos'][$i] . '<br>';
+            }
             
 
 
