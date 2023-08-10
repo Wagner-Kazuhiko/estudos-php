4de7de590cdb1aed166f465ea066634ca05688b7
Wagner <kazu_hiko@hotmail.com>
2023-08-10 19:01:12 -0300

    get a json file with foreach

--- aied/www/html/jornal/tmp/teste.php
+++ aied/www/html/jornal/tmp/teste.php
@@ -5,10 +5,12 @@
             $obj = json_decode(    file_get_contents(dirname(__FILE__).'/exemplo.json') 
             );
 
-            foreach($obj -> contatos as $buffer){
-                print_r($buffer->telefone);
-                echo "<br>";
-            }
+            $arrai = json_decode(    file_get_contents(dirname(__FILE__).'/exemplo.json'), true 
+            );
+
+            print_r($obj);
+            echo "<br>";
+            print_r($arrai);
         ?>
     </body>
 </html>
