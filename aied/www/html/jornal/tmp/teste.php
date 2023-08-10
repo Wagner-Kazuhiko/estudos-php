14dc85c68bb109602c25bbc5bf7a33d686ee93a2
Wagner <kazu_hiko@hotmail.com>
2023-08-10 18:51:10 -0300

    get a json file with foreach

--- aied/www/html/jornal/tmp/teste.php
+++ aied/www/html/jornal/tmp/teste.php
@@ -1,19 +1,12 @@
 <html>
     <body>
         <?php
-            $arr = array(
-                "id" => 1,
-                "nome" => "Kaka",
-                "contatos" => array( 
-                    array("contato1" => "4555-8888"),
-                    array("contato2" => "9999-7777")
-                )
+            
+            $obj = json_decode(    file_get_contents(dirname(__FILE__).'/exemplo.json') 
             );
 
-            $obj = json_decode(json_encode($arr));
-
             foreach($obj -> contatos as $buffer){
-                print_r($buffer);
+                print_r($buffer->telefone);
                 echo "<br>";
             }
         ?>
