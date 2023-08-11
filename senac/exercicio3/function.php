b033dd94f49844e05e711a3df6facdba0b22b91a
Wagner <kazu_hiko@hotmail.com>
2023-08-11 18:21:25 -0300

    function example senac

--- senac/exercicio3/function.php
+++ senac/exercicio3/function.php
@@ -6,4 +6,10 @@
     dizer_ola("aluno");
     dizer_ola("professor");
     dizer_ola("João");
+
+    function soma_dez($num){
+        return 10 + $num;
+    }
+
+    echo "Daqui 10 anos, você terá " . soma_dez(18);
 ?>
