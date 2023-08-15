88475331843795f55de5167d672a5c4019f52b13
Wagner <kazu_hiko@hotmail.com>
2023-08-15 20:59:39 -0300

    apagando linhas do banco

---
+++ senac/banco/apagando-linhas.php
@@ -0,0 +1,27 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$db = new PDO('mysql:host=localhost;dbname=meu_banco;charset=utf8', 'admin', 'Admin@1234');
+$consulta = $db->query('SELECT * FROM usuarios');
+$usuarios = $consulta->fetchAll();
+
+echo '<h1>Lista de usuários</h1>';
+echo '<ul>';
+foreach($usuarios as $u){
+    echo '<li>';
+    echo $u['nome'] . ' - ' . $u['email'];
+    echo '</li>';
+}
+echo '</ul>';
+
+$linhas_afetadas = $db->exec("DELETE FROM `usuarios` WHERE 'id' = 2");
+
+if($linhas_afetadas > 0){
+    echo $linhas_afetadas . ' linhas foram afetadas';
+} else {
+    echo 'Nenhuma linha foi afetada!';
+}
+?>
