e9b4f3f06513ce3e9bdc29d5fa880d329ce7a866
Wagner <kazu_hiko@hotmail.com>
2023-08-19 22:33:21 -0300

    class constructor

--- senac/classes/produto.php
+++ senac/classes/produto.php
@@ -3,11 +3,24 @@
 class Produto {
     public $valor = 10;
     public $nome = 'Pão';
+
+    public function __construct($n, $v){
+        $this -> nome = $n;
+        $this -> valor = $v;
+    }
+
+    public function pegarValor(){
+        return $this -> valor;
+    }
+
+    public function colocarEmpromocao(){
+        $this -> valor = $this -> valor * 0.9;
+    }
 }
 
-$pao = new Produto;
-$leite = new Produto;
-$leite-> nome = 'Leite';
+$pao = new Produto('Pão', 10);
+$leite = new Produto('Leite', 15);
+
 
 
 ?>
