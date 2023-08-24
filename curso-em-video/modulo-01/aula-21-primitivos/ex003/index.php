dbede4d95ef9f4e5417089c4c3bf9173b1fd2bb1
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-08-24 20:53:28 -0300

    tipos primitivos e var_dump()

---
+++ curso-em-video/modulo-01/aula-21-primitivos/ex003/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        //$num = 0b1011;
+        //echo "O valor da varíavel é $num";
+
+        //$v = 300;
+        //var_dump($v);
+
+        //$potencia = 2e3;
+        //echo $potencia;
+
+        //$n = (double) "950";
+        //var_dump($n);
+
+        $casado = true;
+        print "O valor para casado é $casado";
+    ?>
+    
+</body>
+</html>
