362f0050e233c21c97c8fd75867c30f7a0e85c74
Wagner <kazu_hiko@hotmail.com>
2023-08-26 12:49:35 -0300

    curso em video - manipulating strings nowdocs

---
+++ curso-em-video/modulo-01/aula-22-strings/nowdox.php
@@ -0,0 +1,14 @@
+<?php 
+$curso = "PHP";
+$ano = date('Y');
+
+echo <<< 'TESTE'
+Olá galera do $curso!
+        Tudo tranquilo?
+        Como está sendo este ano de $ano ?
+        Abraços! \u{1F596}
+
+TESTE;
+
+
+?>
