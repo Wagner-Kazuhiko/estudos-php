a748b5761436f579d9536c20d8e9a09ff03fa022
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-08-29 20:16:44 -0300

    curso php curso em video - desafio 04

---
+++ curso-em-video/modulo-01/desafio-04/desafio04.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 04</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1></h1>
+    </header>
+
+    <main>
+        <?php 
+            $url = "https://api.coingecko.com/api/v3/simple/price?ids=$coinId&vs_currencies=$currency";
+        ?>
+    </main>
+    
+</body>
+</html>
