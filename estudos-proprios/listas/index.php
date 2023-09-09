c2f89201bd4ef1afd701f3457980750e1d2ff097
Wagner <kazu_hiko@hotmail.com>
2023-09-09 16:45:04 -0300

    teste com arrays 2d

---
+++ estudos-proprios/listas/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$number = 10;
+
+for($i = 1; $i <= $number; $i++){
+    for($j = 1; $j <= $number; $j++){
+        echo "Linha $i ";
+        echo "Exercício $j <br>";
+    }
+    
+}
+
+?>
